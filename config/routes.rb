c9ca4f4727a659bcf92e757b44859b7011404dab
Jose Pantoja <77706927+jpanto21@users.noreply.github.com>
2024-03-12 20:39:21 +0000

    added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  root "photos#index"
+
+  devise_for :users
+
   resources :likes
   resources :follow_requests
   resources :comments
-  root "photos#index"
-  
   resources :photos
-  devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
