f6823bc1d7ef791edcd7c05b013a5a9534ec25c6
Jose Pantoja <77706927+jpanto21@users.noreply.github.com>
2024-03-13 19:56:55 +0000

    added a comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_12_160934) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_11_211734) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
